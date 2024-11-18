2db7d464f0ef9522e368d944f44e3199cec69781
nimbey <83806046+nimbey@users.noreply.github.com>
2024-11-19 01:55:38 +1100

    Enable CORS for frontend

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ connectDB();
 // CORS configuration
 app.use(cors({
     origin: [
+
         'http://127.0.0.1:5501',
         'http://localhost:5501',
         'http://127.0.0.1:5502',
         'http://localhost:5502',
-        'https://your-frontend-domain.onrender.com' // Add your frontend Render URL
+        'https://nimbey.github.io' // Add your frontend Render URL
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
